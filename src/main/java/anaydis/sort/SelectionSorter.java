fdb5fcd0d80b95aab02a9014d017ab8ae517e1b7
sencina <santiago.encina@ing.austral.edu.ar>
2021-08-24 16:16:13 -0300

    cambio en sorters

--- src/main/java/anaydis/sort/SelectionSorter.java
+++ src/main/java/anaydis/sort/SelectionSorter.java
@@ -13,25 +13,22 @@ public class SelectionSorter extends AbstractSorter{
 
     @Override
     public <T> void sort(@NotNull Comparator<T> comparator, @NotNull List<T> list) {
-        int comparaciones = 0;
-        int intercambios = 0;
 
         for (int i = 0; i < list.size(); i++) {
             int min = i;
             for (int j = i+1; j < list.size(); j++) {
-                comparaciones++;
+
                 if (greater(comparator,list,min,j)){
                     min = j;
                 }
             }
             if (min != i) {
                 swap(list, i, min);
-                intercambios++;
+
             }
         }
 
-        System.out.println("Comparaciones = " +comparaciones);
-        System.out.println("Intercambios = " +intercambios);
+
     }
 
 
