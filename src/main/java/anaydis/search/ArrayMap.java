1fd58fabb304c7d85686d69dfb75838dd6a6e4fb
sencina <santiago.encina@ing.austral.edu.ar>
2021-09-09 20:21:16 -0300

    ArrayMap

--- src/main/java/anaydis/search/ArrayMap.java
+++ src/main/java/anaydis/search/ArrayMap.java
@@ -2,20 +2,23 @@ package anaydis.search;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.sql.ClientInfoStatus;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Iterator;
 
 public class ArrayMap<K,V> implements Map<K,V> {
 
+    private final Comparator<K> comparator;
     private ArrayList<K> keys;
     private ArrayList<V> values;
     private int size;
 
-    public ArrayMap() {
+    public ArrayMap(Comparator<K> comparator) {
         this.keys = new ArrayList<>();
         this.values = new ArrayList<>();
         this.size = 0;
+        this.comparator = comparator;
     }
 
     @Override
@@ -30,17 +33,29 @@ public class ArrayMap<K,V> implements Map<K,V> {
 
     @Override
     public boolean containsKey(@NotNull K key) {
-        return false;
+        return indexOf(key)>=0;
     }
 
     @Override
     public V get(@NotNull K key) {
+        int index = indexOf(key);
+        if (index>=0) return values.get(index);
         return null;
     }
 
     @Override
     public V put(@NotNull K key, V value) {
-        return null;
+        int index = indexOf(key);
+        if (index<0){
+            index = -index -1;
+            for (int i = index+1; i < size+1; i++) {
+                keys.set(i,keys.get(i-1));
+                values.set(i,values.get(i-1));
+            }
+            keys.set(index,key);
+        }
+        values.set(index,value);
+        return value;
     }
 
     @Override
@@ -51,21 +66,21 @@ public class ArrayMap<K,V> implements Map<K,V> {
 
     @Override
     public Iterator<K> keys() {
-        return null;
+        return keys.iterator();
     }
 
-    private int indexOf(){
-        return -1;
+    private int indexOf(K key){
+        return find(key,0,size-1);
     }
 
-    private int find(Comparator<K> comparator,K key, int lo, int hi){
+    private int find(K key, int lo, int hi){
 
         if (hi<lo) return -(lo+1);
         int middle = (hi+lo)/2;
         int comp = comparator.compare(key,keys.get(middle));
 
-        if (comp<0) return find(comparator,key,lo,middle-1);
-        else if (comp>0) return find(comparator,key,middle+1,hi);
+        if (comp<0) return find(key,lo,middle-1);
+        else if (comp>0) return find(key,middle+1,hi);
         else return middle;
     }
 }
