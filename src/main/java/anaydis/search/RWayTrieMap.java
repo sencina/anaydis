19567f1e4690985dfb5b577d88df5e2dd8ae39f4
sencina <santiago.encina@ing.austral.edu.ar>
2021-09-16 20:27:15 -0300

    test commits

--- src/main/java/anaydis/search/RWayTrieMap.java
+++ src/main/java/anaydis/search/RWayTrieMap.java
@@ -4,17 +4,16 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.List;
 
-public class RWayTrieMap<V> implements Map<String, V> {
+public class RWayTrieMap <V> implements Map<String,V> {
 
+    private Node<V>head=null;
     private int size;
-    private Node<V> head;
     private V previousValue;
 
     public RWayTrieMap() {
-        this.size = 0;
         this.head = null;
+        this.size = 0;
         this.previousValue = null;
     }
 
@@ -25,104 +24,90 @@ public class RWayTrieMap<V> implements Map<String, V> {
 
     @Override
     public boolean containsKey(@NotNull String key) {
-        return find(head,key,0) != null;
+        return find(head,key,0)!=null;
     }
 
     @Override
     public V get(@NotNull String key) {
-        Node<V> toReturn = find(head,key,0);
-        if (toReturn!=null)return toReturn.value;
-        else return null;
+        Node<V> node=find(head,key,0);
+        if (node.value==null)return null;
+        return node.value;
     }
 
     @Override
     public V put(@NotNull String key, V value) {
-        head = put(head,key,value,0);
+        head=put(head,key,value,0);
         return previousValue;
+    }
+
+    private Node<V> put(Node<V>node, String key, V value, int level) {
+        if (node==null){
+            Node<V>result=new Node<V>(null);
+            if (level < key.length()){
+                int next=(int)key.charAt(level);
+                result.next[next]=put(result.next[next],key,value,level+1);
+            }else{
+                previousValue=result.value;
+                result.value=value;
+                size++;
+            }
+            return result;
+        }
+        if (level==key.length()){
+            previousValue=node.value;
+            node.value=value;
+            return node;
+        }else {
+            int next=(int)key.charAt(level);
+            node.next[next]=put(node.next[next],key,value,level+1);
+            return node;
+        }
     }
 
     @Override
     public void clear() {
-        this.size = 0;
-        this.head = null;
+        head=null;
+        size=0;
     }
 
     @Override
     public Iterator<String> keys() {
-        List<String> toReturn =  new ArrayList<String>();
-        addKeys(head,toReturn);
-        return toReturn.iterator();
+        ArrayList<String>keys=new ArrayList<>();
+        createKlist(head,"",keys);
+        return keys.iterator();
     }
 
-    private Node<V> find(Node<V> node, @NotNull String key, int level) {
-        if (node == null) return null;
-        else if(level == key.length()) return node;
-        else {
-            int nextIndex = key.charAt(level);
-            return find(node.next[nextIndex],key,level+1);
+    private void createKlist(Node<V> node,String key, ArrayList<String> keys) {
+        Node<V>[]next=node.next;
+        for (int i = 0; i < 256; i++) {
+            if (next[i]!=null){
+                Node<V>auxNode=next[i];
+                String posKey=key+(char)i;
+                if (auxNode.value!=null){
+                    keys.add(posKey);
+                }
+                createKlist(auxNode,posKey,keys);
+            }
         }
     }
 
-    private Node<V> put(Node<V> node, @NotNull String key, V value, int level){
-
-        if (node == null){
-
-            Node<V> result = new Node<>();
-
-            if (level<key.length()){
-                int nextIndex = key.charAt(level);
-                result.next[nextIndex] = put(result.next[nextIndex],key,value,level+1);
-            }
-            else {
-                previousValue = result.value;
-                result.value = value;
-                size++;
-            }
-
-            return result;
-        }
-
-        if (level == key.length()) {
-
-            previousValue = node.value;
-            node.value = value;
-            return node;
-        } else {
-            int nextIndex = key.charAt(level);
-            node.next[nextIndex] = put(node.next[nextIndex], key, value, level + 1);
-            return node;
-        }
-
-    }
-
-    private void addKeys(Node<V> node,List<String> list) {
-        if (node == null) return;
-        else {
-            list.add(node.key);
-            for (int i = 0; i < node.next.length; i++) {
-                addKeys(node.next[i],list);
-            }
-        }
+    private Node<V> find(Node<V>node,String key,int level){
+        if (node==null)return null;
+        if (level==key.length())return node;
+        int next=(int)key.charAt(level);
+        return find(node.next[next],key,level+1 );
     }
 
     private class Node<V>{
-
         private V value;
-        private String key;
-        private Node<V>[] next;
-
-        public Node(V value) {
-            this.value = value;
-            this.next = new Node[256];
-            for (int i = 0; i < 256; i++) {
-                next[i] = null;
+        private Node<V>[]next;
+        private Node(V value){
+            this.value=value;
+            next=(Node<V>[])new Node[256];
+            for (int i = 0; i <256 ; i++) {
+                next[i]=null;
             }
         }
-
-        public Node(){
-            new Node<>(null);
-        }
-
-
     }
+
 }
